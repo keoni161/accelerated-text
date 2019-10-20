734d6277e70ce3ec9a16694a5132a5cb1bdfd79f
Žygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2019-10-20 09:22:19 +0300

    feat: processing version with 'parent' parameter

--- api/src/api/scratchpad.clj
+++ api/src/api/scratchpad.clj
@@ -1,31 +1,76 @@
 (ns api.scratchpad
   (:require [clojure.string :as string]))
 
-(defn node-id [prefix index] (keyword (str prefix (or index ""))))
+(defn update-semantic-graph
+  ([{:keys [nodes relations] :as sg}
+    new-nodes new-relations]
+   (assoc sg
+          :nodes (set (concat nodes new-nodes))
+          :relations (set (concat relations new-relations))))
+  ([sg new-nodes] (update-semantic-graph sg new-nodes [])))
 
-(defn update-semantic-graph [sg nodes relations]
-  (assoc sg
-         (concat (get sg :nodes) nodes)
-         (concat (get sg :relations) relations)))
+(defmulti process-node (fn [_ _ node] (-> node :type (string/lower-case) keyword)))
 
-(defmulti process-node (fn [_ node] (-> node :type (string/lower-case) keyword)))
+(defmethod process-node :cell [sg parent {field :name}]
+  (update-semantic-graph
+   sg
+   [{:type :data :field (keyword field) :id (str "data-" field)}]
+   [[(:id parent) :has (str "data-" field)]]))
 
-(defmethod process-node :cell [sg {field :name}]
-  (update-semantic-graph sg
-                         [{:type :data :id (node-id (first field) 0)}]
-                         []))
+(defmethod process-node :dictionary-item-modifier
+  [sg parent {child :child word-id :name}]
+  (let [modifier-node {:type :modifier :dictionary (keyword word-id)
+                       :id (str "modifier-" word-id)}
+        {child-nodes :nodes child-rels :relations}
+        ;;`parent` here would be `modifier-node` but we are reversing
+        ;; graph relation, see comment bellow
+        (process-node sg parent child)]
+    (update-semantic-graph
+     sg
+     (conj child-nodes modifier-node)
+     (conj child-rels
+           [;; The way relation here is constructed is not good.
+            ;; In Blockly we get MODIFIER(Adjective)->CELL(Noun) structure.
+            ;; Which is inverse from what we should have in semantic graph:
+            ;; DATA->MODIFIER. Thus a reversing of relation is needed.
+            ;; Reversing itself depends on child-nodes being in a *vector* data structure.
+            (-> child-nodes first :id)
+            :modifier (:id modifier-node)]))))
 
-(defmethod process-node :dictionary-item-modifier [sg {child :child item-id :itemId word-id :name}]
-  (let [{child-nodes :nodes child-relations :relations} (process-node sg child)]
+;;In Blockly structure this is root AMR node - event. `dictionaryItem` is an
+;;unfortunate name.
+(defmethod process-node :dictionary-item [sg parent {type :type name :name}]
+  (let [amr-node {:type :amr
+                  :name (keyword name)
+                  :id (str "amr-" name)}]
     (update-semantic-graph sg
-                           (conj child-nodes {:type :modifier :id (node-id item-id 0) :dictionary word-id})
-                           (conj child-relations []))))
+                           [amr-node]
+                           [[(:id parent) :amr (:id amr-node)]])))
+
+(defn process-amr-arg [sg parent arg-name roles]
+  (reduce (fn [m child] (process-node m parent child))
+          sg
+          (:children (some #(when (= arg-name (:name %)) %) roles))))
+
+(defmethod process-node :amr [sg parent {roles      :roles
+                                         dictionary :dictionaryItem}]
+  (let [amr  (process-node sg parent dictionary)
+        ;;See comment on `process-node :dictionary-item-modifier`
+        amr-node (-> amr :nodes first)
+        arg0 (process-amr-arg amr amr-node "agent" roles)
+        arg1 (process-amr-arg arg0 amr-node "co-agent" roles)]
+    (update-semantic-graph
+     arg1
+     (concat (:nodes amr) (:nodes arg0) (:nodes arg1)))))
+
+(defmethod process-node :segment [sg parent {children :children}]
+  (let [segment {:type :segment :id "segment"}] ;TODO ids for segments
+    (reduce (fn [m child] (process-node m segment child))
+            (update-semantic-graph sg
+                                   [segment]
+                                   [[(:id parent) :has (:id segment)]])
+            children)))
 
 (defn parse-document-plan [{segments :segments}]
-  (map-indexed
-    (fn [idx child]
-      (process-node (update-semantic-graph {:nodes [] :relations []}
-                                           [{:type :segment :id (node-id "s" idx)}]
-                                           [])
-                    child))
-    segments))
+  (reduce (fn [m seg] (process-node m {:type :document :id "document-1"} seg))
+          {} segments))
--- api/test/api/scratchpad_test.clj
+++ api/test/api/scratchpad_test.clj
@@ -2,23 +2,22 @@
   (:require [api.scratchpad :as sut]
             [clojure.test :refer [deftest is]]))
 
-
 (def amr-dp {:type "Document-plan"
              :segments
-             [{:children [{:roles
-                           [{:children [{:name   "good"
-                                         :type   "Dictionary-item-modifier"
-                                         :child  {:name "authors" :type "Cell"}
-                                         :itemId "good"}]
-                             :name     "agent"}
-                            {:children [{:name "title" :type "Cell"}]
-                             :name     "co-agent"}
-                            {:children [nil] :name "theme"}]
-                           :dictionaryItem {:name   "written"
-                                            :type   "Dictionary-item"
-                                            :itemId "written"}
-                           :type           "AMR"
-                           :conceptId      "author"}]
+             [{:children
+               [{:roles [{:children [{:name   "good"
+                                      :type   "Dictionary-item-modifier"
+                                      :child  {:name "authors" :type "Cell"}
+                                      :itemId "good"}]
+                          :name     "agent"}
+                         {:children [{:name "title" :type "Cell"}]
+                          :name     "co-agent"}
+                         {:children [] :name "theme"}]
+                 :dictionaryItem {:name   "written"
+                                  :type   "Dictionary-item"
+                                  :itemId "written"}
+                 :type           "AMR"
+                 :conceptId      "author"}]
                :type     "Segment"}]})
 
 (def tiny-dp
@@ -31,23 +30,29 @@
      :type  "Segment"}]
    :type  "Document-plan"})
 
+(def segs-dp
+  {:segments
+   [{:children [{:name "title" :type "Cell"} {:name "title" :type "Cell"}]
+     :type  "Segment"}
+    {:children [{:name "author" :type "Cell"}]
+     :type  "Segment"}]
+   :type  "Document-plan"})
+
 (def tiny-semantic
-  {:nodes     [{:type :segment :id :s1}
-               {:type :data :filed :title :id :t1}
-               {:type :modifier :dictionary :good1 :id :g1}]
-   :relations [[:s1 :has :t1]
-               [:p1 :ancillary :g1]]})
+  {:nodes     #{{:type :segment :id "segment"}
+                {:type :data :field :title :id "data-title"}
+                {:type :modifier :dictionary :good :id "modifier-good"}}
+   :relations #{["document-1" :has "segment"]
+                ["segment" :has "data-title"]
+                ["data-title" :modifier "modifier-good"]}})
 
 (def amr-semantic
-  {:nodes     [{:type :segment :id :s1}
-               {:type :data :filed :title :id :t1}
-               {:type :data :filed :author :id :p1}
-               {:type :modifier :dictionary :good1 :id :g1}
-               {:type :event :dictionary :authorship1 :id :a1}]
-   :relations [[:s1 :has :a1]
-               [:a1 :arg0 :p1]
-               [:a1 :arg1 :t1]
-               [:p1 :ancillary :g1]]})
+  {:nodes     #{{:type :segment :id "segment"}
+               {:type :dictionary-item :name :written :id "dictionary-written"}
+               {:type :modifier :dictionary :good :id "modifier-good"}
+               {:type :data :field :authors :id "data-authors"}
+               {:type :data :field :title :id "data-title"}}
+   :relations #{["document-1" :has "segment"]}})
 
 (deftest dp->semantic
   (is (= tiny-semantic (sut/parse-document-plan tiny-dp)))
