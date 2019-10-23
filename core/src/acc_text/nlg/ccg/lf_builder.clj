c77435111180e744f601746bc4871ec17f96e9df
Zygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2019-10-23 13:52:57 +0300

    chore: experiment with LF building

--- core/src/acc_text/nlg/ccg/lf_builder.clj
+++ core/src/acc_text/nlg/ccg/lf_builder.clj
@@ -1,23 +1,31 @@
 (ns acc-text.nlg.ccg.lf-builder
+  (:require [acc-text.nlg.grammar-generation.translate :as translate])
   (:import java.io.StringReader
            org.jdom.input.SAXBuilder
-           org.jdom.output.XMLOutputter)
-  (:require [acc-text.nlg.grammar-generation.translate :as translate]))
+           [org.jdom.output Format XMLOutputter]))
 
 (def she-buys-it-lf
   "<xml> <lf> <satop nom=\"w1:action\"> <prop name=\"buy\"/> <diamond mode=\"tense\"> <prop name=\"pres\"/> </diamond> <diamond mode=\"Actor\"> <nom name=\"w0:animate-being\"/> <prop name=\"pro3f\"/> <diamond mode=\"num\"> <prop name=\"sg\"/> </diamond> </diamond> <diamond mode=\"Patient\"> <nom name=\"w2:thing\"/> <prop name=\"pro3n\"/> <diamond mode=\"num\"> <prop name=\"sg\"/> </diamond> </diamond> </satop> </lf> </xml>")
 
-(def tiny-grammar (Grammar. (-> "grammars/tiny/grammar.xml" java.io.File. .toURI .toURL)))
-
 (defn parse [lf]
-  (.build (SAXBuilder.) (StringReader. lf)))
+  (.getRootElement 
+    (.build (SAXBuilder.) (StringReader. lf))))
 
 (defn xml-print [dom]
-  (.outputString (XMLOutputter.) dom))
+  (.outputString (XMLOutputter. (Format/getPrettyFormat) ) dom))
 
 (defn construct-lf []
   (translate/build-lf
    (translate/build-satop "X")))
 
 (defn lex-words [g]
-  (let [w (.getWords (.lexicon g))] (doseq [k (.keySet w)] (prn k ":" (.get w k)))))
+  (let [lf-xml (translate/build-custom-el {:name "lf"})
+        l (.lexicon g)
+        words (.getWords l)]
+    (doseq [word (.keySet words)]
+      (prn "WORD: " (.getForm word))
+      (doseq [sign (.getSignsSorted (.getSignsFromWord l word))]
+        (let [lf (.getLF (.getCategory sign))]
+          (translate/add-content lf-xml (.toXml lf)))))
+    (print (xml-print lf-xml))
+    lf-xml))
